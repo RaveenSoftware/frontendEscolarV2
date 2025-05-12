3d4409418dee87117a9a82fa9acc02da0403744d
RaveenSoftware <124215601+RaveenSoftware@users.noreply.github.com>
2025-05-12 18:44:31 -0500

    Configure Backend Connection

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,12 +6,11 @@ import { Observable, BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8080/api/auth/login';
+  private apiUrl = 'api/auth/login';
   private authStatus = new BehaviorSubject<boolean>(this.isAuthenticated());
 
   constructor(private http: HttpClient) { }
 
-  // Realiza la petición de login
   login(numeroDocumento: string, password: string): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
     const body = new URLSearchParams();
@@ -21,26 +20,20 @@ export class AuthService {
     return this.http.post(this.apiUrl, body.toString(), { headers });
   }
 
-  // Maneja el logout
   logout() {
     localStorage.removeItem('authToken');
-    this.authStatus.next(false); // Cambia el estado de autenticación
+    this.authStatus.next(false);
   }
 
-  // Verifica si el usuario está autenticado
   isAuthenticated(): boolean {
     return !!localStorage.getItem('authToken');
   }
 
-  // Obtiene el estado de autenticación
   getAuthStatus(): Observable<boolean> {
     return this.authStatus.asObservable();
   }
 
-  // Actualiza el estado de autenticación
   updateAuthStatus(status: boolean) {
     this.authStatus.next(status);
   }
 }
-
-
